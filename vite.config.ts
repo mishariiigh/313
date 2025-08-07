52333de6c72167fadf9423f1eb9cc9c4b614cbd2
mishariiigh <mishariiigh@hotmail.com>
2025-08-07 14:30:21 -0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   root: path.resolve(__dirname, 'client'),
   base: './',
   build: {
-    outDir: path.resolve(__dirname, 'dist'),
+    outDir: 'dist', 
     emptyOutDir: true,
   },
   plugins: [react()],
