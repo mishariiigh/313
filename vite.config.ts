1e31e6bf5ad58975da2e196d6c56e65301d7c95a
mishariiigh <mishariiigh@hotmail.com>
2025-08-08 10:56:46 -0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,6 @@ export default defineConfig({
             'firebase/storage'
           ],
           react: ['react', 'react-dom'],
-          vendor: ['lodash', 'axios', 'zod']
         }
       }
     }
