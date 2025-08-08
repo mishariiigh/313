e3fe18d556f7ea808e5033b60f7393231ab04f72
mishariiigh <mishariiigh@hotmail.com>
2025-08-08 15:13:22 -0400

    update

--- client/src/lib/queryClient.ts
+++ client/src/lib/queryClient.ts
@@ -12,18 +12,11 @@ export async function apiRequest(
   url: string,
   data?: unknown | undefined,
 ): Promise<Response> {
-  const headers: Record<string, string> = {};
-  
-  if (data) {
-    headers["Content-Type"] = "application/json";
-  }
-
   const res = await fetch(url, {
     method,
-    headers,
+    headers: data ? { "Content-Type": "application/json" } : {},
     body: data ? JSON.stringify(data) : undefined,
     credentials: "include",
-    mode: "cors", // Ensure CORS mode is enabled
   });
 
   await throwIfResNotOk(res);
